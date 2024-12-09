308b592c892e35e252e4be09a045f05b623d0522
Camille310 <82250164+Camille310@users.noreply.github.com>
2024-12-09 17:20:24 +0800

    Add files via upload

---
+++ model/model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# Define model architecture
+class SetConv(nn.Module):
+    def __init__(self,  predicate_feats,  hid_units):
+        super(SetConv, self).__init__()
+
+        self.predicate_mlp1 = nn.Linear(predicate_feats, hid_units)
+        self.predicate_mlp2 = nn.Linear(hid_units, hid_units)
+        self.out_mlp1 = nn.Linear(hid_units * 1, hid_units)
+        self.out_mlp2 = nn.Linear(hid_units, 1)
+
+    def forward(self, predicates, predicate_mask):
+
+
+        hid_predicate = F.relu(self.predicate_mlp1(predicates))
+        hid_predicate = F.relu(self.predicate_mlp2(hid_predicate))
+        hid_predicate = hid_predicate * predicate_mask
+        hid_predicate = torch.sum(hid_predicate, dim=1, keepdim=False)
+        predicate_norm = predicate_mask.sum(1, keepdim=False)
+        hid_predicate = hid_predicate / predicate_norm
+
+
+        hid = hid_predicate
+        hid = F.relu(self.out_mlp1(hid))
+        out = torch.sigmoid(self.out_mlp2(hid))
+        return out
