e02ea7abe93edb66100942a96fca21609ca4e208
Yutong Xie <82250164+yutongxie7@users.noreply.github.com>
2025-01-16 20:33:25 +0800

    Add files via upload

--- eval.py
+++ eval.py
@@ -4,11 +4,11 @@ import os
 import torch
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-
+import pandas as pd
 from model.util import *
 from model.data import get_train_datasets, load_data, make_dataset
 from model.model import SetConv
-
+import csv
 def unnormalize_torch(vals, min_val, max_val):
     # 反归一化
     vals = (vals * (max_val - min_val)) + min_val
@@ -24,18 +24,35 @@ def unnormalize_torch(vals, min_val, max_val):
         vals = torch.exp(vals)
 
     return vals
+def unnormalize_torch_pre(vals, min_val, max_val,min_val_ar, max_val_ar):
+    first_elements, second_elements = zip(*vals)
+    tensor1 = torch.tensor(first_elements)
+    tensor2 = torch.tensor(second_elements)
 
-def qerror_loss(preds, targets, min_val, max_val):
-    qerror = []
-    preds = unnormalize_torch(preds, min_val, max_val)
-    targets = unnormalize_torch(targets, min_val, max_val)
+    # 反归一化
+    tensor1 = (tensor1 * (max_val - min_val)) + min_val
+    # 判断是否需要处理负值
+    if min_val < 0:
+        # 对于负值，使用相应的反归一化逻辑
+        neg_mask = tensor1 < 0
+        # 对于负值，使用 1 / exp(-vals) 来恢复
+        tensor1 = torch.where(neg_mask, -torch.exp(-tensor1), torch.exp(tensor1))
+    else:
+        # 对于非负值，直接使用指数还原
+        tensor1 = torch.exp(tensor1)
 
-    for i in range(len(targets)):
-        if (preds[i] > targets[i]).cpu().data.numpy()[0]:
-            qerror.append(abs(preds[i] - targets[i]))
-        else:
-            qerror.append(abs(targets[i] - preds[i]))
-    return torch.mean(torch.cat(qerror))
+    tensor2 = (tensor2 * (max_val_ar - min_val_ar)) + min_val_ar
+    # 判断是否需要处理负值
+    if min_val_ar < 0:
+        # 对于负值，使用相应的反归一化逻辑
+        neg_mask = tensor2 < 0
+        # 对于负值，使用 1 / exp(-vals) 来恢复
+        tensor2 = torch.where(neg_mask, -torch.exp(-tensor2), torch.exp(tensor2))
+    else:
+        # 对于非负值，直接使用指数还原
+        tensor2 = torch.exp(tensor2)
+
+    return tensor1,tensor2
 
 
 def predict(model, data_loader, cuda):
@@ -45,7 +62,7 @@ def predict(model, data_loader, cuda):
     model.eval()
     for batch_idx, data_batch in enumerate(data_loader):
 
-        predicates,  targets, predicate_masks = data_batch
+        predicates,  targets,targets_ar, predicate_masks = data_batch
 
         if cuda:
             predicates,targets = predicates.cuda(),targets.cuda()
@@ -69,46 +86,73 @@ def predict(model, data_loader, cuda):
 
 def eval(workload_name, num_queries, batch_size, hid_units, cuda):
 
-    num_materialized_samples = 1000
-    dicts, column_min_max_vals, min_val, max_val, labels_train, labels_test, max_num_predicates, train_data, test_data = get_train_datasets(
+    num_materialized_samples = 200
+    dicts, column_min_max_vals, min_val, max_val,min_val_ar, max_val_ar, labels_train, labels_train_ar,labels_test,label_test_ar, max_num_predicates, train_data, test_data = get_train_datasets(
         num_queries, num_materialized_samples)
     column2vec, op2vec = dicts
-    model = load_model("model/setconv_model.pth", 8, 256, False)
+    model = load_model("model/setconv_model1.pth", 8, 256, False)
 
 
     file_name = "workloads/" + workload_name
-    joins, predicates, tables, samples, label = load_data(file_name, num_materialized_samples)
+    joins, predicates, tables, samples, label,label_ar = load_data(file_name, num_materialized_samples)
 
-    # Get feature encoding and proper normalization
-    # samples_test = encode_samples(tables, samples, table2vec)
+
     predicates_test = encode_data(predicates, column_min_max_vals, column2vec, op2vec)
-    labels_test, _, _ = normalize_labels(label, min_val, max_val)
+    labels_test, min_val, max_val = normalize_labels(label)
 
+    labels_test_ar, min_val_ar, max_val_ar = normalize_labels(label_ar)
 
     max_num_predicates = max([len(p) for p in predicates_test])
 
 
     # Get test set predictions
-    test_data = make_dataset(predicates_test, labels_test, max_num_predicates)
+
+    test_data = make_dataset(predicates_test, labels_test,labels_test_ar, max_num_predicates)
     test_data_loader = DataLoader(test_data, batch_size=batch_size)
 
     preds_test, t_total = predict(model, test_data_loader, cuda)
-    print("Prediction time per test sample: {}".format(t_total / len(labels_test) * 1000))
-
     # Unnormalize
-    preds_test_unnorm = unnormalize_labels(preds_test, min_val, max_val)
+    preds_test_unnorm = unnormalize_torch_pre(preds_test, min_val, max_val,min_val_ar,max_val_ar)
+    preds_rspn=preds_test_unnorm[0]
+    preds_ar=preds_test_unnorm[1]
     # Print metrics
     # Write predictions
     file_name = "results/predictions_" + workload_name + ".csv"
     os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    rspn_pr_result= [int(x) for x in preds_rspn.tolist()]
+    ar_pr_result = [int(x) for x in preds_ar.tolist()]
+    print(rspn_pr_result)
+    print(ar_pr_result)
+    label = [int(x) for x in label]
+    label_ar=[int(x) for x in label_ar]
+    print("labels_rspn",label)
+    print("laebls_ar",label_ar)
     with open(file_name, "w") as f:
         for i in range(len(preds_test_unnorm)):
-            f.write(str(preds_test_unnorm[i]) + "," + str(label[i]) + "\n")
+            f.write(str(preds_rspn[i]) + "," + str(preds_ar[i]) + "\n")
     print("Prediction time per test sample: {}".format(t_total / len(labels_test) * 1000))
     print("evaling finished")
+    data = pd.read_csv('results/predictions_synthetic.csv', header=None)
+    # 创建空的列表用于存储行数
+
+
+    # 假设 rspn_pr_result 和 label 列表是这样的
+
+
+    # 创建并写入 CSV 文件
+    folder_path = './model_RSPN'
+    file_path = os.path.join(folder_path, 'output.csv')
+    with open(file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["rspn_pr_result", "label"])  # 写入表头
+        for i in range(len(rspn_pr_result)):
+            writer.writerow([rspn_pr_result[i], label[i]])  # 写入每行数据
+
+
 def load_model(model_path, predicate_feats, hid_units, cuda):
     model = SetConv(predicate_feats, hid_units)
     model.load_state_dict(torch.load(model_path))
+
     if cuda:
         model.cuda()
     model.eval()  # Set the model to evaluation mode
